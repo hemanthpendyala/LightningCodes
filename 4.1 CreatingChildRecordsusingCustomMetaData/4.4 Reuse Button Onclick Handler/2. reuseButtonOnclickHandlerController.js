a79d0ad46906ddf6d8781d22146018c69c6c803e
hemanthpendyala <hemanth.pendyala40@gmail.com>
2019-11-04 18:22:12 -0800

    Delete 2. reuseButtonOnclickHandlerController.js

--- 4.1 CreatingChildRecordsusingCustomMetaData/4.4 Reuse Button Onclick Handler/2. reuseButtonOnclickHandlerController.js
+++
@@ -1,8 +0,0 @@
-({
-	Controller01 : function(component, event, helper) {
-		var whichOne = event.getSource().getLocalId();
-        alert(whichOne);
-        var forAttributes = event.getSource().get("v.name");
-        alert(forAttributes);
-	}
-})
